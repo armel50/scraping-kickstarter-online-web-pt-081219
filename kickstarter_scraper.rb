8cf210fac408ac590fbc86dde182ebe929d732c9
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-26 05:34:19 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ def create_project_hash
   
   projects_array =  kickstarter.css(".project.grid_4")
   
-  p projects_array.first.css("a")
+  p projects_array.first.css("a").text
   # projects_array.each do |el|
     
   # end
